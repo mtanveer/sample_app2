50801ad1fc802552dbd83cc299932eb0620605d0
Tanveer Bashir <tan.m.bashir@gmail.com>
2013-01-19 19:09:30 -0500

    sample app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd5151acccc35462355636e3255efe6a048b428895aace9dc62cb4048da44703426baf5796abcb25c86c579adf1a0049778127d888834fe04c9b5899ff084d18f'
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+	factory :user do 
+		name 					"Tanveer Bashir"
+		email					"mtanver@example.com"
+		password  				"foobar"
+		password_confirmation 	"foobar"
+
+	end	
+end
